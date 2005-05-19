8f765c413911d355dd95a312e31deaf1166206f9
Søren Sandmann Pedersen <sandmann@daimi.au.dk>
2005-05-19 20:33:55 +0000

    Add forgotten region.h

---
+++ region.h
@@ -0,0 +1,10 @@
+/*
+ * Dummy header to work around the fact that Picture.c does
+ *
+ *  #include "region.h"
+ *
+ * but the files is actually part of libX11 and installed in
+ * /usr/include/X11/region.h
+ *
+ */
+#include <X11/region.h>
